a30af9f5229578a16d7ecfce3bf50e9fefbcac05
photoeb <63953970+photoeb@users.noreply.github.com>
2020-08-03 18:53:03 +0300

    eapteka

---
+++ src/test/java/tests/Eapteka.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Eapteka {
+    @Test
+
+    void EaptekaAuthTest () {
+
+        open("https://eapteka.ru");
+
+        $(by("data-tab-id", "auth")).click();
+        $(by("name", "LOGIN")).setValue("");
+        $(by("name", "PASSWORD")).setValue("");
+        $(byText("Запомнить")).click();
+        $(byText("Войти")).click();
+
+        $(by("class", "htop__links right")).shouldHave(text("Выход"));
+
+
+    }
+}
