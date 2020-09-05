f9c1cfeb1f9082c54291bdaa8b0537b0b11c19d3
photoeb <63953970+photoeb@users.noreply.github.com>
2020-09-05 12:42:22 +0300

    l9hw

--- src/test/java/tests/lesson1/YandexTests.java
+++ src/test/java/tests/lesson1/YandexTests.java
@@ -1,17 +1,22 @@
 package tests.lesson1;
 
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 
 class YandexTests {
 
     @Tag("for_jenkins")
     @Test
-    void selenideSearchTest() {
+    void lepraSearchTest() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.headless = true;
         open("https://ya.ru");
 
         $("#text").setValue("лепра").pressEnter();
